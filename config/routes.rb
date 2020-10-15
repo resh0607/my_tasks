7068380aab0d426b34dc6a7171dc922d36de7c60
Ivan Reshetnyak <resh0607@gmail.com>
2020-10-15 20:22:57 +0300

    <upd> update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'projects#index'
 
-  resources :projects, only: :index
+  resources :projects, only: :index do
+    resources :todos, only: :update
+  end
+
+  resources :todos, only: :create, shallow: true
 end
