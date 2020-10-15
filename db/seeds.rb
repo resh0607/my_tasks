677b204299a71b4a6a99eb09426299db081764dd
Ivan Reshetnyak <resh0607@gmail.com>
2020-10-15 12:54:32 +0300

    <upd> update seed method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,9 @@
+require 'yaml'
 
-projects = Project.create!([
-  {title: 'Семья'},
-  {title: 'Работа'},
-  {title: 'Прочее'}
-])
+data = YAML.load_file('db/seeds.yml')
 
-todos = Todo.create!([
-  {text: 'Купить молоко',                           project: projects[0]},
-  {text: 'Заменить масло в двигателе до 23 апреля', project: projects[0]},
-  {text: 'Отправить письмо бабушке',                project: projects[0], is_completed: true},
-  {text: 'Заплатить за квартиру',                   project: projects[0]},
-  {text: 'Забрать обувь из ремонта',                project: projects[0]},
-  {text: 'Купить молоко',                           project: projects[0]},
-
-  {text: 'Позвонить заказчику',                     project: projects[1], is_completed: true},
-  {text: 'Отправить документы',                     project: projects[1], is_completed: true},
-  {text: 'Заполнить отчет',                         project: projects[1]},
-
-  {text: 'Позвонить другу',                         project: projects[2]},
-  {text: 'Подготовиться к поездке',                 project: projects[2]}
-])
+projects = data['projects']
+projects.each do |project|
+  created_project = Project.create!(title: project['title'])
+  created_project.todos.create!(project['todos'])
+end
