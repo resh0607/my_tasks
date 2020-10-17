a5b68fefaeac0b251dfd2681f8589d12225de5a3
Ivan Reshetnyak <resh0607@gmail.com>
2020-10-17 19:51:19 +0300

    <upd> update Project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ApplicationRecord
   has_many :todos, dependent: :destroy
 
-  validates :title, presence: true,
-                    uniqueness: { case_sensitive: false }
+  validates :title, length: { maximum: 25 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /[[:alnum:]]+/,
+                              message: "Add at least one letter or number" }
+
 end
